329e9c206a4fd041b500e4840b0e6900df48238c
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2018-02-22 20:16:16 -0300

    Finish some code refactoring, to solve issues that @kstanislawska pointed. Also fixes for vertical levels, included as a dictionary in gfs.yml. #9

--- Grib2nc/grib2nc.py
+++ Grib2nc/grib2nc.py
@@ -18,7 +18,6 @@ def _get_GRIB_date(grib):
 
     return date_anl
 
-
 def _get_GRIB_offset(grib):
     """
         Calculate offset in ours from analises date 
@@ -30,13 +29,11 @@ def _get_GRIB_offset(grib):
 
     return offset
 
-
 def _get_timestep(offset, step=3):
     """
         Returns the timestep based on the offset from analises and also step size 
     """
     return int(offset // step)
-
 
 def _push_GRIB_NC(gribfile, grib_fn, nc_file, yml,t=0):
     """
@@ -53,20 +50,20 @@ def _push_GRIB_NC(gribfile, grib_fn, nc_file, yml,t=0):
             if t == 0 and i == 0:
                 suc = create_NC(nc_file, grib)
                 if suc == True:
-                    suc = append_NC(nc_file, _var, grib, yml['time_step'])
+                    suc = append_NC(nc_file, _var, grib, yml['time_step'], yml['levels'])
                 else:
                     try:
-                        suc = append_NC(nc_file, _var, grib, yml['time_step'])
+                        suc = append_NC(nc_file, _var, grib, yml['time_step'], yml['levels'])
                     except:
-                        string = grib_fn.split(".")[0] + ":" + d0.strftime('%Y-%m-%d_%H:%M') + ':' \
-                            + grib_fn.split(".")[2] + '.nc'
+                        string = grib_fn.split(".")[0]+":"+d0.strftime('%Y-%m-%d_%H:%M')+':'\
+                            + grib_fn.split(".")[2]+'.nc'
                         print("Error creating dimensions on file: %s" % (string))
                         exit(1)
             else:
-                suc = append_NC(nc_file, _var, grib, yml['time_step'])
+                suc = append_NC(nc_file, _var, grib, yml['time_step'], yml['levels'])
                 if suc == False:
-                    string = grib_fn.split(":")[0] + ":" + d0.strftime('%Y-%m-%d_%H:%M') + ':' \
-                        + grib_fn.split(":")[2] + '.nc'
+                    string = grib_fn.split(":")[0]+":"+d0.strftime('%Y-%m-%d_%H:%M')+':'\
+                        +grib_fn.split(":")[2]+'.nc'
                     print("Error appending %s on file: %s" % (_var[0], string))
                     exit(1)
 
@@ -75,21 +72,21 @@ def _push_GRIB_NC(gribfile, grib_fn, nc_file, yml,t=0):
     else:
         return(False, 'Null')
 
-
 def NC_is_new(date_anl, grib_fn, nc_file, fn_config):
     """
-        Verify if the nice file already exists or is a new one
+        Verify if the nc file already exists or is a new one
     """
     try:
         partial_fn = grib_fn.split(":")
         nc_fn = partial_fn[
-            fn_config.index('{name}')] + ":" + date_anl.strftime('%Y-%m-%d_%H:%M') + ':' + partial_fn[
-            fn_config.index('{member}')] + '.nc'
+                fn_config.index('{name}')]+":"\
+                    +date_anl.strftime('%Y-%m-%d_%H:%M')+':'+\
+                        partial_fn[fn_config.index('{member}')]+'.nc'
 
     except:
         partial_fn = grib_fn.split(":")
-        nc_fn = partial_fn[
-            0] + ":" + date_anl.strftime('%Y-%m-%d_%H:%M') + ':' + partial_fn[2][:10] + '.nc'
+        nc_fn = partial_fn[0]+":"+date_anl.strftime('%Y-%m-%d_%H:%M')+\
+                                            ':'+partial_fn[2][:10]+'.nc'
     else:
         print("Unknown filename format, %s" % (grib_fn))
         exit(1)
@@ -97,11 +94,11 @@ def NC_is_new(date_anl, grib_fn, nc_file, fn_config):
         t = 1
         try:
             nc_file = nc_file
-            timestep = len(nc_file.variable['times'])
+            timestep = len(nc_file.variable['Times'])
         except:
             try:
                 nc_file = Dataset(nc_fn, 'r+')
-                timestep = len(nc_file.variable['times'])
+                timestep = len(nc_file.variable['Times'])
             except:
                 timestep = 0
     else:
@@ -110,7 +107,6 @@ def NC_is_new(date_anl, grib_fn, nc_file, fn_config):
         timestep = 0
 
     return(timestep, nc_file, t)
-
 
 def create_NC_dimension(nc_file, shape, size=0):
     """
@@ -125,72 +121,61 @@ def create_NC_dimension(nc_file, shape, size=0):
     except:
         return False
 
-
 def create_NC(nc_file, grib, comp_lvl=6):
     """
         Create the initial variables and dimensions
     """
     latlons = grib.latlons()
     latlon = np.array(latlons)
-    lat = latlon[0, :, :]
-    lon = latlon[1, :, :]
-
+    lat = latlon[0]
+    lon = latlon[1]
     nc_dm = create_NC_dimension(nc_file, lat.shape)
-
     if nc_dm == True:
-        xlat = nc_file.createVariable('XLAT', 'u4', ('south_north', 'west_east'), zlib=True,
-                                      least_significant_digit=3, complevel=int(comp_lvl))
-        xlon = nc_file.createVariable('XLONG', 'u4', ('south_north', 'west_east'), zlib=True,
-                                      least_significant_digit=3, complevel=int(comp_lvl))
-        xlat = lat
-        xlon = lon
-        times = nc_file.createVariable('times', 'S2', ('Time'), zlib=True, complevel=6)
+        nc_file.createVariable('XLAT', 'u2', ('south_north', 'west_east'),\
+              zlib=True, least_significant_digit=3, complevel=int(comp_lvl))
+        nc_file.createVariable('XLONG', 'u2', ('south_north', 'west_east'),\
+              zlib=True, least_significant_digit=3, complevel=int(comp_lvl))
+        nc_file.createVariable('Times', 'S2', ('Time'), zlib=True, complevel=6)
+
+        nc_file.variables['XLAT'][:]  = lat
+        nc_file.variables['XLONG'][:] = lon
+
+        nc_file.START_DATE = grib.analDate.strftime('%Y-%m-%d_%H:%M')
+        nc_file.HISTORY = "Created by Belgingur grib2nc utility at " + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M')
+
         nc_file.sync()
         return True
-
     else:
         return False
 
-
-def append_NC(nc_file, _var, grib, step,comp_lvl=6):
+def append_NC(nc_file, _var, grib, step,levels, comp_lvl=6):
     """
         This function append a new timstep to the nc file if it already exists, or create the variable
     """
     offset = _get_GRIB_offset(grib)
     timestep = _get_timestep(offset, step)
     d0 = _get_GRIB_date(grib)
+    nc_file['Times'][timestep]=(d0+datetime.timedelta(hours=offset)).strftime("%Y-%m-%d_%H:00:00")
     try:
         if _var[3] == 'sfc':
-            nc_var = nc_file.variables[_var[0]]
-            nc_var[timestep, :, :] = grib.values
-            time = nc_file.variables['times']
-            time[timestep] = (
-                d0 + datetime.timedelta(hours=offset)).strftime("%Y-%m-%d_%H")
+            nc_file.variables[_var[0]][timestep, :, :] = grib.values
             return True
         else:
-            nc_var = nc_file.variables[_var[0]]
-            nc_var[timestep, :, :, grib.level] = grib.values
-            time = nc_file.variables['times']
-            time[timestep] = (
-                d0 + datetime.timedelta(hours=offset)).strftime("%Y-%m-%d_%H")
+            level = levels.get(grib.level, 0)
+            nc_file.variables[_var[0]][timestep, level,:, :] = grib.values
             return True
 
     except:
         if _var[3] == 'sfc':
-            nc_var = nc_file.createVariable(_var[0], _var[1], ('Time', 'south_north', 'west_east'),
-                                            zlib=True, least_significant_digit=int(np.abs(np.log10(_var[2]))), complevel=int(comp_lvl))
-            nc_var[timestep, :, :] = grib.values
-            time = nc_file.variables['times']
-            time[timestep] = (
-                d0 + datetime.timedelta(hours=offset)).strftime("%Y-%m-%d_%H")
+            nc_file.createVariable(_var[0], _var[1], ('Time', 'south_north', 'west_east'),\
+            zlib=True, least_significant_digit=int(np.abs(np.log10(_var[2]))), complevel=int(comp_lvl))
+            nc_file.variables[_var[0]][timestep, :, :] = grib.values
             return True
         else:
-            nc_var = nc_file.createVariable(_var[0], _var[1], ('Time', 'south_north', 'west_east', 'bottom_top'),
-                                            zlib=True, least_significant_digit=int(np.abs(np.log10(_var[2]))), complevel=int(comp_lvl))
-            nc_var[timestep, :, :, grib.level] = grib.values
-            time = nc_file.variables['times']
-            time[timestep] = (
-                d0 + datetime.timedelta(hours=offset)).strftime("%Y-%m-%d_%H")
+            level = levels.get(grib.level, 0)
+            nc_file.createVariable(_var[0], _var[1], ('Time', 'bottom_top', 'south_north', 'west_east'),\
+            zlib=True, least_significant_digit=int(np.abs(np.log10(_var[2]))), complevel=int(comp_lvl))
+            nc_file.variables[_var[0]][timestep, level, :, :] = grib.values
             return True
 
     else:
@@ -199,9 +184,10 @@ def append_NC(nc_file, _var, grib, step,comp_lvl=6):
 
 #######################################
 parser = argparse.ArgumentParser(description="This script transform GRIB2 files in to NetCDF")
-parser.add_argument('GRIB_PATH', help="Path to a single file or folder.", action='store')
-parser.add_argument('OUTPUT_PATH', help="Output path for nc file.", action='store')
-parser.add_argument('-c', help="Configuration file(YAML) for especific model", action='store',required=True, dest='yml')
+parser.add_argument('-g', help="Path to a single grib file or folder", action='store',required=True, dest='GRIB_PATH')
+parser.add_argument('-o', help="Output path for nc file.", action='store', required=True, dest='OUTPUT_PATH')
+parser.add_argument('-c', help="Configuration file(YAML) for especific model", \
+                                            action='store',required=True, dest='yml')
 args=parser.parse_args()
 
 path_grib = args.GRIB_PATH
